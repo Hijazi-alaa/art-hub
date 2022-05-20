0317e6a173f01452dceab5df571c91e2494490ab
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-20 11:51:56 +0000

    Display different links for loggedin/loggedout users and added signout functionalty

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,55 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+  
+    const handleSignOut = async () => {
+      try {
+        await axios.post("dj-rest-auth/logout/");
+        setCurrentUser(null);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    const loggedInLinks = <>{currentUser?.username}</>
+    const addPostIcon = (
+        <NavLink
+          className={styles.NavLink}
+          activeClassName={styles.Active}
+          to="/posts/create"
+        >
+          <i class="fa-solid fa-circle-plus"></i>Add post
+        </NavLink>
+      );
+    
+      const loggedInLinks = (
+        <>
+        <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/" ><i className="fas fa-home"></i>Home</NavLink>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/liked"
+          >
+            <i class="fa-solid fa-heart-circle-check"></i>Liked
+          </NavLink>
+          <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+            <i className="fas fa-sign-out-alt"></i>Sign out
+          </NavLink>
+          <NavLink
+            className={styles.NavLink}
+            to={`/profiles/${currentUser?.profile_id}`}
+          >
+            <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+          </NavLink>
+        </>
+      );
     const loggedOutLinks = (
     <>
     <NavLink className={styles.NavLink} activeClassName={styles.Active}  to="/signin"><i class="fa-solid fa-right-to-bracket"></i><br></br>Sign in</NavLink>
@@ -23,10 +66,11 @@ const NavBar = () => {
                     <img src={logo} alt="logo" height="45"/>
                 </Navbar.Brand>
             </NavLink>
+            {currentUser && addPostIcon}
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/" ><i className="fas fa-home"></i><br></br>Home</NavLink>
+                    
                     {currentUser ? loggedInLinks : loggedOutLinks }
                 </Nav>
             </Navbar.Collapse>
