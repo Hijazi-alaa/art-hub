8e5e027651d8ce83da1a51a65aabdb3b37eb51c1
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-22 12:46:15 +0000

    Add editing a comment form so users can edit their own comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { axiosRes } from '../../api/axiosDefaults';
@@ -6,12 +6,14 @@ import Avatar from '../../components/Avatar';
 import { OptionsDropdown } from '../../components/OptionsDropdown';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from "../../styles/Comment.module.css";
-
+import EditCommentForm from './EditCommentForm';
 
 const Comment = (props) => {
 
     const { profile_id, profile_image, owner, updated_at, content,
         id, setPost, setComments } = props;
+
+    const [showEditForm, setShowEditForm] = useState(false);
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
@@ -32,28 +34,31 @@ const Comment = (props) => {
         }catch(err){}
     } 
 
-  return (
-    <div>
-        <hr />
-        <Media>
-            <Link to={`/profiles/${profile_id}`} >
-                <Avatar src={profile_image} />
+    return (
+        <>
+          <hr />
+          <Media>
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar src={profile_image} />
             </Link>
             <Media.Body className="align-self-center ml-2">
-                <span className={styles.Owner} >
-                    {owner}
-                    </span>
-                <span className={styles.Date} >
-                    {updated_at}
-                </span>
+              <span className={styles.Owner}>{owner}</span>
+              <span className={styles.Date}>{updated_at}</span>
+              {showEditForm ? (
+                <EditCommentForm id={id} profile_id={profile_id}
+                content={content} profileImage={profile_image}
+                setComments={setComments} setShowEditForm={setShowEditForm} />
+              ) : (
                 <p>{content}</p>
+              )}
             </Media.Body>
-            {is_owner && (
-                < OptionsDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+            {is_owner && !showEditForm && (
+              <OptionsDropdown handleEdit={() => setShowEditForm(true)}
+                handleDelete={handleDelete} />
             )}
-        </Media>
-    </div>
-  )
+          </Media>
+        </>
+      );
 }
 
 export default Comment
