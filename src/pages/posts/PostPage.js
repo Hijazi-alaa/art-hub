0b73cbc4b5b54a2695d728135c1afc524f007c0e
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-22 11:40:20 +0000

    Add the ability to comment with its form and style

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,11 +8,17 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
-
+import AddCommentForm from "../comments/AddCommentForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
     const {id} = useParams();
     const [post, setPost] = useState({ results: [] });
+
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: []});
 
     useEffect(() => {
         const handleMount = async () => {
@@ -33,7 +39,13 @@ function PostPage() {
       <Col className="py-2 p-0 p-lg-2" lg={10}>
         <Post {...post.results[0]} setPosts={setPost} postPage={true} />
         <Container className={appStyles.Content}>
-          Comments
+        {currentUser ? (
+        <AddCommentForm profile_id={currentUser.profile_id}
+        profileImage={profile_image} post={id}
+        setPost={setPost} setComments={setComments} />
+        ) : comments.results.length ? (
+          "Comments"
+          ) : null}
         </Container>
       </Col>
     </Row>
