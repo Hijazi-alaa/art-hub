b4eb015c14bdd3b617614760314cfa91338cd24b
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-22 15:41:25 +0000

    Added edit profile, password and username forms to the profile and resricted that only profile owner can edit

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -17,6 +17,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
 import { fetchMorePosts } from "../../utils/utils";
 import NoResults from "../../assets/no-results.jpg";
+import { ProfileEditDropdown } from "../../components/OptionsDropdown";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -51,6 +52,7 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
+    {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image className={styles.ProfileImage}
