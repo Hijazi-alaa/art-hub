791e259e89e1a99337223348d833cac7c16c22c4
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-22 13:01:51 +0000

    add infinitescroll functionality to comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -11,6 +11,9 @@ import Post from "./Post";
 import AddCommentForm from "../comments/AddCommentForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMorePosts } from "../../utils/utils";
 
 function PostPage() {
     const {id} = useParams();
@@ -37,33 +40,48 @@ function PostPage() {
         handleMount();
     }, [id]);
 
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={10}>
-        <Post {...post.results[0]} setPosts={setPost} postPage={true} />
-        <Container className={appStyles.Content}>
-        {currentUser ? (
-        <AddCommentForm profile_id={currentUser.profile_id}
-        profileImage={profile_image} post={id}
-        setPost={setPost} setComments={setComments} />
-        ) : comments.results.length ? (
-          "Comments"
-          ) : null}
-          {comments.results.length ? (
-            comments.results.map(comment => (
-              <Comment key={comment.id} {...comment}
-              setPost={setPost} setComments={setComments} />
-            ))
-          ) : currentUser ? (
+    return (
+      <Row className="h-100">
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+          <p>Popular profiles for mobile</p>
+          <Post {...post.results[0]} setPosts={setPost} postPage />
+          <Container className={appStyles.Content}>
+            {currentUser ? (
+              <AddCommentForm
+                profile_id={currentUser.profile_id}
+                profileImage={profile_image}
+                post={id}
+                setPost={setPost}
+                setComments={setComments}
+              />
+            ) : comments.results.length ? (
+              "Comments"
+            ) : null}
+            {comments.results.length ? (
+              <InfiniteScroll
+                children={comments.results.map((comment) => (
+                  <Comment
+                    key={comment.id}
+                    {...comment}
+                    setPost={setPost}
+                    setComments={setComments}
+                  />
+                ))}
+                dataLength={comments.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!comments.next}
+                next={() => fetchMorePosts(comments, setComments)}
+              />
+            ) : currentUser ? (
             <span>There are no comments on this posts yet, be the first to add a comment!</span>
-          ) : (
+            ) : (
             <span>There are no comments on this post yet...</span>
-          )
-        }
-        </Container>
-      </Col>
-    </Row>
-  );
-}
+            )}
+          </Container>
+        </Col>
+      </Row>
+    );
+  }
+  
 
 export default PostPage;
